b7b5e78820e5311fa3ac9070aeb5b047bc2b8b7d
simon <simon@clearfly.co.uk>
2024-02-21 22:29:03 +0000

    spl metadata challenge

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -4,12 +4,14 @@ import {
     createMetadataAccountV3, 
     CreateMetadataAccountV3InstructionAccounts, 
     CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+    DataV2Args,
+    MPL_TOKEN_METADATA_PROGRAM_ID
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import { createSignerFromKeypair, signerIdentity, publicKey, publicKeyBytes } from "@metaplex-foundation/umi";
+import { PublicKey } from "@solana/web3.js";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("3vgiHEwpBz68kUr73RWBX5kwSvmoA6dYJ8gpJ4VceJ79")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -19,29 +21,49 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
     try {
+
+        let metadata = umi.eddsa.findPda(MPL_TOKEN_METADATA_PROGRAM_ID, [
+                Buffer.from("metadata"),
+                publicKeyBytes(MPL_TOKEN_METADATA_PROGRAM_ID),
+                publicKeyBytes(mint)
+            ]
+        )
+
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            metadata: metadata,
+            mint: mint,
+            mintAuthority: signer,
+            payer: signer,
+            updateAuthority: signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: 'WBA_META',
+            symbol: 'WBA_META',
+            uri: '',
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+             data:data,
+             isMutable:true,
+             collectionDetails:null
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+             umi,
+             {
+                 ...accounts,
+                 ...args
+             }
+        );
 
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
+        let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
+        console.log(result);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
