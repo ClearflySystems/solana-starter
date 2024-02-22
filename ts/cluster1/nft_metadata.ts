8b22b1fc26feba9c772aa7a2f501f5a7a7ce8268
simon <simon@clearfly.co.uk>
2024-02-22 22:08:25 +0000

    NFT Image Upload and Mint

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,15 +1,15 @@
 import wallet from "../wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
+import { irysUploader  } from "@metaplex-foundation/umi-uploader-irys"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
-const bundlrUploader = createBundlrUploader(umi);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
+umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
@@ -17,27 +17,30 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your image URI: ", myUri);
+        const image = "https://arweave.net/BFhSa6nI_axxuKW61su2VaXFT7hHek7grbD1g4TopGA"
+        const metadata = {
+            name: "RUG_Doctor",
+            symbol: "WBA_RUG",
+            description: "The Rug doctor will come and clean out your rugs",
+            image: image,
+            attributes: [
+                {trait_type: 'ruggable', value: '0'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your image Metadata URI: ", myUri);
+
+        // https://arweave.net/phoPSiRxbQAFebrOY3tU7QwMaU-P_jQzOf_VyDwVPZg
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -16,11 +16,25 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    let tx = createNft(umi, {
+        name: "RUG_Doctor",
+        symbol: "WBA_RUG",
+        uri: "https://arweave.net/phoPSiRxbQAFebrOY3tU7QwMaU-P_jQzOf_VyDwVPZg",
+        sellerFeeBasisPoints: percentAmount(3),
+        mint
+    });
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
+
+    // https://explorer.solana.com/tx/KWFiNNz8drmYyoEzqBYF37D2g1rUwqjioMargYjtdu938UhPtVCqLy42HhxoPYbyPAJ2mQHtqT1FnVGry9hKQuv?cluster=devnet
+    // Mint Address:  E3WwtLDz7BcxUDa9aP64QfiZ6PNbKGPd9XHuqXDjfXyp
+    
+    // The Rug
+    // https://explorer.solana.com/address/E3WwtLDz7BcxUDa9aP64QfiZ6PNbKGPd9XHuqXDjfXyp?cluster=devnet
+
+
 })();
